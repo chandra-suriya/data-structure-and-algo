71a27efa68025a987df6bb4e455c21f8867f3454
chandra suriya <chandrasuriya305@gmail.com>
2022-03-04 12:15:50 +0530

    peak element in mount added

--- searching_problems.cpp
+++ searching_problems.cpp
@@ -144,3 +144,109 @@ int main()
     return 0;
 }
 
+// find the peak index in mountain array
+int peak(int ar[7])
+{
+    int start=0;
+    int end=6;
+    while(start<=end)
+    {
+        int mid = start+(end-start)/2;
+        if(ar[mid]>ar[mid+1] && ar[mid]>ar[mid-1])
+        {
+            return mid;
+        }
+        else if(ar[mid]<ar[mid+1])
+        {
+            start=mid+1;
+        }
+        else if(ar[mid]<ar[mid-1])
+        {
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+
+// find in mountain array
+
+
+#include <iostream>
+using namespace std;
+int peak(int ar[7])
+{
+    int first=0,last=6;
+    while(first<=last)
+    {
+        int mid=first+(last-first)/2;
+        if(ar[mid]>ar[mid+1] && ar[mid]>ar[mid-1])
+        {
+            return mid;
+        }
+        else if(ar[mid]>ar[mid+1])
+        {
+            last=mid-1;
+        }
+        else if(ar[mid]>ar[mid-1])
+        {
+            first=mid+1;
+        }
+    }
+    return -1;
+}
+int orderignosticsearch(int ar[7],int start,int end,int tar)
+{
+    bool asc,desc;
+    if(ar[start]<ar[end])
+    {
+        asc=true;
+    }
+    while(start<=end)
+    {
+        int mid = start+(end-start)/2;
+        if(ar[mid]==tar)
+        {
+            return mid;
+        }
+        if(asc)
+        {
+        if(ar[mid]<tar)
+        {
+            start=mid+1;
+        }
+        else if(ar[mid]>tar)
+        {
+            end=mid-1;
+        }
+        }
+        else
+        {
+           if(ar[mid]<tar)
+        {
+            end=mid-1;
+        }
+        else if(ar[mid]>tar)
+        {
+            start=mid+1;
+        }
+        }
+    }
+    return-1;
+}
+
+int main() {
+   int ar[]={1,2,3,4,5,3,1};
+   int tar =3;
+   int peakele = peak(ar);
+   int mini = orderignosticsearch(ar,0,peakele,tar);
+   if(mini==-1)
+   {
+       cout<<orderignosticsearch(ar,peakele+1,4,tar);
+   }
+   else
+   {
+       cout<<mini;
+   }
+   
+    return 0;
+}
