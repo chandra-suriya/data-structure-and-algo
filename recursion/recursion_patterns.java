50b19ed5a0670928db9dcea7a5ce3fff333d0dee
chandra suriya <chandrasuriya305@gmail.com>
2022-03-29 10:06:51 +0530

    recursion rev

--- recursion/recursion_patterns.java
+++ recursion/recursion_patterns.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class recursion_patterns {
     public static void main(String[] args) {
-       //1.star pattern problem
+       //1.star pattern problem reverse
        printstarrev(3,3,0,0);
+       //2.star pattern original
+       printstarori(3,3,0,0);
+       Scanner input = new Scanner(System.in);
+       System.out.println(input.nextLine());
+       input.close();
     }
     static void printstarrev(int r,int c,int i, int j){
         // base case
@@ -15,5 +21,19 @@ public class recursion_patterns {
         else{
         System.out.println();
         printstarrev(r,c-1,0,j+1);}
+    }
+    static void printstarori(int r,int c,int i,int j){
+        if(j==r){
+            return;
+        }
+        if(i<c){
+         printstarori(r,c,i+1,j);
+         System.out.print("* ");
+        }
+        else{
+        printstarori(r,c-1,0,j+1);
+        System.out.println();
+    }
+
     }
 }
